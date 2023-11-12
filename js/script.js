fcc4b7a357091a7638fb0d5eec1115aa545e08da
Patipoll <ezboomgg123@gmail.com>
2023-11-12 15:04:05 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var playButton = document.getElementById("play-button");
+var homeButton = document.getElementById("Home-button");
+
+playButton.addEventListener('click', () => {
+    window.location.href = "chat.html";
+});
+
+
