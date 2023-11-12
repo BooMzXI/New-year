d05b97cd6fcfb2c12ad8783b2fd9bc606ed84592
Patipoll <ezboomgg123@gmail.com>
2023-11-12 18:04:30 +0700

    light

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 var playButton = document.getElementById("play-button");
 var homeButton = document.getElementById("Home-button");
+var circles = document.getElementsByClassName("circlesClass");
+
+Array.from(circles).forEach(circle => {
+    var isGreen = false;
+
+    circle.addEventListener('click', function() {
+        isGreen = !isGreen;
+        if (isGreen) {
+            this.style.backgroundColor = "green";
+        } else {
+            this.style.backgroundColor = "red";
+        }
+    });
+});
 
 playButton.addEventListener('click', () => {
     window.location.href = "chat.html";
