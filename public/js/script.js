b1f5c89efa2a3bf6365f91634ae620226de23c3b
BankTNBD <bankthanabadee2007@icloud.com>
2023-11-25 21:29:05 +0700

    🆕 : New server script!!

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,14 @@
 var playButton = document.getElementById("play-button");
 var homeButton = document.getElementById("Home-button");
 var circles = document.getElementsByClassName("circlesClass");
+var msg = document.getElementById("msg");
+
+let server = "http://localhost:8000";
 
 Array.from(circles).forEach(circle => {
     var isGreen = false;
 
-    circle.addEventListener('click', function() {
+    circle.addEventListener('click', function () {
         isGreen = !isGreen;
         if (isGreen) {
             this.style.backgroundColor = "green";
@@ -25,10 +28,22 @@ function redirectTo(page) {
 
 function toggleMenu() {
     var menuContent = document.getElementById("all-button");
-    
     if (menuContent.style.display === "none" || menuContent.style.display === "") {
         menuContent.style.display = "block";
     } else {
         menuContent.style.display = "none";
     }
+}
+
+function writeMsg() {
+    fetch(server+"/write", {
+        method: "POST",
+        body: JSON.stringify({
+            text: msg.value
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    msg.value = "";
 }
